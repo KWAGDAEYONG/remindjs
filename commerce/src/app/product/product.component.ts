ae2eb5f264936faabc5430e55a10dd60258083e5
kwagdaeyong <dayong7387@hanmail.net>
2017-12-06 00:59:03 +0900

    clear

--- commerce/src/app/product/product.component.ts
+++ commerce/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../shared/product";
 
 @Component({
   selector: 'dany-product',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  product : Product = {
+    id : '1',
+    name : 'Angular Note',
+    price : 2000,
+    info : 'lorem',
+    avg_stars : 4,
+    total_reviews : 200
+  }
+  stars : number[] = Array(5).fill(0).map((x,i) => (i+1 <= this.product.avg_stars) ? 1:0);
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onStarClicked(e:Event){
+    console.log(e.target);
+  }
+
+  onCartClicked(e:Event){
+    console.log(e.target);
+  }
+
+  onDetailClicked(e:Event){
+    console.log(e.target);
+  }
 }
